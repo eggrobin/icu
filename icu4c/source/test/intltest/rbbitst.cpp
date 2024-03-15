9d861e4779b35b265a2eda8165ef80ee3efb6889
Robin Leroy <egg.robin.leroy@gmail.com>
2024-03-15 11:59:54 +0100

    🐒

--- icu4c/source/test/intltest/rbbitst.cpp
+++ icu4c/source/test/intltest/rbbitst.cpp
@@ -15,6 +15,7 @@
 #if !UCONFIG_NO_BREAK_ITERATION
 
 #include <algorithm>
+#include <array>
 #include <set>
 #include <sstream>
 #include <stdio.h>
@@ -22,6 +23,7 @@
 #include <string.h>
 #include <utility>
 #include <vector>
+#include <string_view>
 
 #include "unicode/brkiter.h"
 #include "unicode/localpointer.h"
@@ -4179,7 +4181,8 @@ void RBBITest::RunMonkey(BreakIterator *bi, RBBIMonkeyKind &mk, const char *name
         if (numIterations == -1 && loopCount % 10 == 0) {
             // If test is running in an infinite loop, display a periodic tic so
             //   we can tell that it is making progress.
-            fprintf(stderr, ".");
+            constexpr std::array<std::string_view, 5> monkeys{"🙈", "🙉", "🙊", "🐵", "🐒"};
+            fprintf(stderr, monkeys[m_seed % monkeys.size()].data());
         }
         // Save current random number seed, so that we can recreate the random numbers
         //   for this loop iteration in event of an error.
