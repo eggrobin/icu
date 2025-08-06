8ae9f9767efc2e40868f492ee92ecb2149cde7e2
Robin Leroy <egg.robin.leroy@gmail.com>
2025-08-06 16:13:43 +0200

    Errors in the test

--- icu4c/source/test/intltest/usettest.cpp
+++ icu4c/source/test/intltest/usettest.cpp
@@ -4484,10 +4484,10 @@ void UnicodeSetTest::TestUTS61Examples() {
     for (const auto [expression, expected] :
          std::vector<std::pair<std::u16string_view, std::variant<UnicodeSet, UErrorCode>>>{
              {uR"(\N{SPACE})", UnicodeSet(uR"([\u0020])", status)},
-             {uR"([[\u0000-\u007F]-\N{TILDE}])", UnicodeSet(uR"([\u0000-\u007D\u007E])", status)},
-             {uR"([\N{SPACE}-~])", UnicodeSet(uR"([\u0000-\u007D\u007E])", status)},
+             {uR"([[\u0000-\u007F]-\N{TILDE}])", UnicodeSet(uR"([\u0000-\u007D\u007F])", status)},
+             {uR"([\N{SPACE}-~])", UnicodeSet(uR"([\u0020-\u007E])", status)},
              {uR"([[\u0000-\u007F]&\N{TILDE}])", U_MALFORMED_SET},
-             {uR"([\N{SPACE}-\N{TILDE}])", UnicodeSet(uR"([\u0000-\u007D\u007E])", status)},
+             {uR"([\N{SPACE}-\N{TILDE}])", UnicodeSet(uR"([\u0020-\u007E])", status)},
          }) {
         expectUnicodeSetExpression(expression, expected);
     }
