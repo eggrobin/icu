bb0e7edc42c5573ca610f1dbc0037188e90a94ec
Robin Leroy <egg.robin.leroy@gmail.com>
2026-06-30 16:53:31 +0200

    Unbump the version so the tools can read it

--- icu4c/source/common/rbbidata.h
+++ icu4c/source/common/rbbidata.h
@@ -59,7 +59,7 @@ ubrk_swap(const UDataSwapper *ds,
 U_NAMESPACE_BEGIN
 
 // The current RBBI data format version.
-static const uint8_t RBBI_DATA_FORMAT_VERSION[] = {7, 0, 0, 0};
+static const uint8_t RBBI_DATA_FORMAT_VERSION[] = {6, 0, 0, 0};
 
 /*  
  *   The following structs map exactly onto the raw data from ICU common data file. 
